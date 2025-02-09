c21dfaf67e2e3e2c3fa1917f384cf3658a2a9a73
Md Abdul Quaiyum Chowdhury <Md.Abdul.Quaiyum.Chowdhury@bodo.kommune.no>
2025-02-09 23:17:54 +0100

    first

---
+++ validation.js
@@ -0,0 +1,4 @@
+function details(info){
+    return `my name is : ${info.name}. my age is : ${info.age}`; /* this is called template syntex method. it is a object */
+}
+console.log(details({name : 'Abdul', age : 34}))
